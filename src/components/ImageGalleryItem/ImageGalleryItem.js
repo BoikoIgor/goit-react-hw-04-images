4087591a522de9ef4e78a1a7f56b79514983fe4c
Igor Boiko <igorboiko@gmail.com>
2023-05-04 23:06:13 +0300

    change to hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,43 +1,57 @@
 import { ModalPhoto } from 'components/Modal/Modal';
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    // image: null,
-    // alt: '',
-    isModal: false,
+// export class ImageGalleryItem extends Component {
+//   state = {
+//     isModal: false,
+//   };
+//   toggleModal = () => {
+//     this.setState({ isModal: !this.state.isModal });
+//   };
+//   render() {
+//     const { previewURL, photoURL, alt } = this.props;
+//     return (
+//       <li>
+//         <img
+//           src={previewURL}
+//           alt={alt}
+//           className="ImageGalleryItem"
+//           onClick={this.toggleModal}
+//         />
+//         <ModalPhoto
+//           isModal={this.state.isModal}
+//           onClose={this.toggleModal}
+//           photoURL={photoURL}
+//           alt={alt}
+//         />
+//       </li>
+//     );
+//   }
+// }
+
+export const ImageGalleryItem = ({ previewURL, photoURL, alt }) => {
+  const [isModal, setIsModal] = useState(false);
+  const toggleModal = () => {
+    setIsModal(!isModal);
   };
-  // setImage = () => {
-  //   const { photoURL, alt } = this.props;
-  //   this.setState({ image: photoURL, alt: alt });
-  // };
-  // closeModal = () => {
-  //   this.setState({ image: null, alt: '' });
-  // };
-  toggleModal = () => {
-    this.setState({ isModal: !this.state.isModal });
-  };
-  render() {
-    const { previewURL, photoURL, alt } = this.props;
-    return (
-      <li>
-        <img
-          src={previewURL}
-          alt={alt}
-          className="ImageGalleryItem"
-          onClick={this.toggleModal}
-        />
-        <ModalPhoto
-          isModal={this.state.isModal}
-          onClose={this.toggleModal}
-          photoURL={photoURL}
-          alt={alt}
-        />
-      </li>
-    );
-  }
-}
+  return (
+    <li>
+      <img
+        src={previewURL}
+        alt={alt}
+        className="ImageGalleryItem"
+        onClick={toggleModal}
+      />
+      <ModalPhoto
+        isModal={isModal}
+        onClose={toggleModal}
+        photoURL={photoURL}
+        alt={alt}
+      />
+    </li>
+  );
+};
 ImageGalleryItem.propTypes = {
   previewURL: PropTypes.string,
   photoURL: PropTypes.string.isRequired,
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
-// import './styles.css';
 
 const theme = {
   colors: {
